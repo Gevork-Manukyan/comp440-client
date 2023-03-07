c06b090b827e6a675fe3bfd6eaa1ddc0d4db09b6
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-03-06 20:01:15 -0800

    adds routing

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import '../../resources/themes.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from "../Authentication/LoginPage/LoginPage"
 
 function App() {
   return (
     <div className="App">
-      ``
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<></>} ></Route>
+          <Route path="/login" element={<LoginPage />} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
