7f74de27c4d492ee3d9ea09d59b703a7311db2f1
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-03-12 21:06:58 -0700

    updates register form

--- src/Components/Authentication/RegisterPage/RegisterPage.js
+++ src/Components/Authentication/RegisterPage/RegisterPage.js
@@ -29,9 +29,29 @@ export default function Register() {
     }
     
     return (
-        <div className='Register'>
+        <div className='RegisterPage'>
              <form onSubmit={handleOnSubmit}>
                 <div className="authForm">
+                    <div id='name-area'>
+                        <fieldset className="authFieldset">
+                            <label>
+                                <p id="signup-firstName-title" className="form-title">First</p>
+                                <input name="firstName" type={"firstName"} onChange={handleOnChange} />
+                            </label>
+                        </fieldset>
+                        <fieldset className="authFieldset">
+                            <label>
+                                <p id="signup-lastName-title" className="form-title">Last</p>
+                                <input name="lastName" type={"lastName"} onChange={handleOnChange} />
+                            </label>
+                        </fieldset>
+                    </div>
+                    <fieldset className="authFieldset">
+                        <label>
+                            <p id="signup-username-title" className="form-title">Username</p>
+                            <input name="username" type={"username"} onChange={handleOnChange} />
+                        </label>
+                    </fieldset>
                     <fieldset className="authFieldset">
                         <label>
                             <p id="signup-email-title" className="form-title">Email</p>
