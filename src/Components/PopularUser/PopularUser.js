a0b1cc9a8fc17ebbec424e5c6fbf647a68e170c6
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-05-07 13:31:17 -0700

    finishes PopularUser

--- src/Components/PopularUser/PopularUser.js
+++ src/Components/PopularUser/PopularUser.js
@@ -1,9 +1,36 @@
-
+import { useEffect, useState } from "react"
+import apiClient from "../../services/apiClient"
 
 export default function PopularUser() {
-    return (
-        <div className="PopularUser">
+    const [data, setData] = useState()
 
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await apiClient.getPopularUsers()
+            setData(res.data)
+        }
+
+        fetchData()
+    }, [])
+
+    return (
+        <div className="PopularUser table-wrapper">
+            <table className="styled-table">
+              <thead className='Header'>
+                <tr>
+                    <th>User</th>
+                    <th>Item Count</th>
+                </tr>
+              </thead>
+              <tbody>
+              {data?.map((item, index) => (
+                  <tr key={index}>
+                  <td>{item.username}</td>
+                  <td>{item.total_items}</td>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
         </div>
     )
 }
