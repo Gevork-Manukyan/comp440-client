1cd4078d8e89e0406b5b83325617d72bdd906dcb
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-05-06 17:37:26 -0700

    adds hamburger

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
+import './Navbar.css'
 import { Link } from 'react-router-dom'
 import apiClient from '../../services/apiClient'
-import './Navbar.css'
+import { slide as Menu } from 'react-burger-menu'
+
 
 export default function Navbar({ authenticated, setAuthenticated }) {
     const handleLogout = () => {
@@ -11,10 +13,24 @@ export default function Navbar({ authenticated, setAuthenticated }) {
     return (
         <div className="Navbar">
             <nav className="navbar">
-                <div className='item-section logged-in-items'>
-                    <div className="nav-item"><Link to="/">Home</Link></div>
-                    <div className="nav-item"><Link to="/search">Search</Link></div>
-                    <div className="nav-item"><Link to="/reviews">Reviews</Link></div>
+                <div className={`item-section logged-in-items ${authenticated ? "" : "hidden"}`}>
+                    <Menu>
+                        <div className="nav-item"><Link to="/">Home</Link></div>
+                        <div className="nav-item"><Link to="/search">Search</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Reviews</Link></div>
+                        
+                        <div className="nav-item"><Link to="/reviews">Expensive Items</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Two Items Same Day</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Excellent/Good Item</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Popular User</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Same Friends</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Not Excellent User</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Nice Reviewers</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Mean Reviewers</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Good Producers</Link></div>
+                        <div className="nav-item"><Link to="/reviews">Friend Users</Link></div>
+                    </Menu>
+
                 </div>
                 <div className='item-section auth-section'>
                     {authenticated ? 
