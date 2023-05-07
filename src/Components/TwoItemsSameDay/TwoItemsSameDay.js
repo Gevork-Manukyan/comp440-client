fae400da5ae717b8ef63402d26e6581ab7f30a3c
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-05-07 01:26:55 -0700

    creates TwoItemsSameDay

--- src/Components/TwoItemsSameDay/TwoItemsSameDay.js
+++ src/Components/TwoItemsSameDay/TwoItemsSameDay.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react"
+import apiClient from "../../services/apiClient"
 
 
 export default function TwoItemsSameDay() {
-    return (
-        <div className="TwoItemsSameDay">
 
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await apiClient.getTwoItemsDiffCategorySameDay()
+            setData(res.data)
+        }
+
+        fetchData()
+    }, [])
+    
+    return (
+        <div className="TwoItemsSameDay table-wrapper">
+             <table className="styled-table">
+              <thead className='Header'>
+                <tr>
+                    <th>User</th>
+                </tr>
+              </thead>
+              <tbody>
+              {data?.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item}</td>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
         </div>
     )
 }
