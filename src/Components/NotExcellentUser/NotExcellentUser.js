462154b9b8770be01ff7fff05f91fd0ef8e41283
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-05-08 19:17:07 -0700

    creates NotExcellentUser

--- src/Components/NotExcellentUser/NotExcellentUser.js
+++ src/Components/NotExcellentUser/NotExcellentUser.js
@@ -1,9 +1,35 @@
-
+import { useEffect, useState } from "react"
+import apiClient from "../../services/apiClient"
 
 export default function NotExcellentUser() {
-    return (
-        <div className="NotExcellentUser">
 
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await apiClient.getNotExcellentUsers()
+            setData(res.data)
+        }
+
+        fetchData()
+    }, [])
+    
+    return (
+        <div className="NotExcellentUser table-wrapper">
+            <table className="styled-table">
+              <thead className='Header'>
+                <tr>
+                    <th>User</th>
+                </tr>
+              </thead>
+              <tbody>
+              {data?.map((item, index) => (
+                  <tr key={index}>
+                  <td>{item}</td>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
         </div>
     )
 }
