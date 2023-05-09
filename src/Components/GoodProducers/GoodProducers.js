1e36f866df16ef239597a20e0c56adf0468d21a2
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-05-08 20:00:25 -0700

    creates GoodProducers

--- src/Components/GoodProducers/GoodProducers.js
+++ src/Components/GoodProducers/GoodProducers.js
@@ -1,10 +1,36 @@
-
+import { useEffect, useState } from "react"
+import apiClient from "../../services/apiClient"
 
 
 export default function GoodProducers() {
-    return (
-        <div className="GoodProducers">
 
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await apiClient.getGoodProducers()
+            setData(res.data)
+        }
+
+        fetchData()
+    }, [])
+    
+    return (
+        <div className="GoodProducers table-wrapper">
+            <table className="styled-table">
+              <thead className='Header'>
+                <tr>
+                    <th>User</th>
+                </tr>
+              </thead>
+              <tbody>
+              {data?.map((item, index) => (
+                  <tr key={index}>
+                  <td>{item}</td>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
         </div>
     )
 }
