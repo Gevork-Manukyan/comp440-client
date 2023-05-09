80107795c00f18e1e422bd9826bfa151979b3897
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2023-05-08 19:22:50 -0700

    creates NiceReviewers

--- src/Components/NiceReviewers/NiceReviewers.js
+++ src/Components/NiceReviewers/NiceReviewers.js
@@ -1,9 +1,35 @@
-
+import { useEffect, useState } from "react"
+import apiClient from "../../services/apiClient"
 
 export default function NiceReviewers() {
-    return (
-        <div className="NiceReviewers">
 
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await apiClient.getNiceReviewers()
+            setData(res.data)
+        }
+
+        fetchData()
+    }, [])
+    
+    return (
+        <div className="NiceReviewers table-wrapper">
+            <table className="styled-table">
+              <thead className='Header'>
+                <tr>
+                    <th>User</th>
+                </tr>
+              </thead>
+              <tbody>
+              {data?.map((item, index) => (
+                  <tr key={index}>
+                  <td>{item}</td>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
         </div>
     )
 }
